12a9fb06d5f2fd36b4477b862c6a8de094eb20d4
kim <ksh251024@naver.com>
2024-09-26 15:09:47 +0900

    feat : 변수명 카멜 케이스로 변경, 파라미터명 변경

--- src/main/java/com/copypay/model/User.java
+++ src/main/java/com/copypay/model/User.java
@@ -4,9 +4,9 @@ import lombok.Data;
 
 @Data
 public class User {
-    private int NO;
-    private String ID;
-    private String PW;
-    private String EMAIL;
+    private int no;
+    private String id;
+    private String pw;
+    private String email;
 
 }
--- src/main/java/com/copypay/repository/mapper/UserMapper.java
+++ src/main/java/com/copypay/repository/mapper/UserMapper.java
@@ -5,5 +5,5 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserMapper {
-    User findByUsername(String ID);
+    User findByUsername(String username);
 }
