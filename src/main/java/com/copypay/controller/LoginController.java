36f67c0289bdbbf31a89b2066e154f8c1c1bd990
kim <ksh251024@naver.com>
2024-09-30 10:35:12 +0900

    feat : 컨트롤러 엔드포인트 추가

--- src/main/java/com/copypay/controller/LoginController.java
+++ src/main/java/com/copypay/controller/LoginController.java
@@ -1,11 +1,18 @@
 package com.copypay.controller;
 
+import com.copypay.dto.MailDto;
+import com.copypay.service.MailService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@AllArgsConstructor
 public class LoginController {
+    private final MailService mailService;
 
     @GetMapping("/")
     public String login(@RequestParam(value="error", required = false) String error){
@@ -16,4 +23,20 @@ public class LoginController {
     public String home() {
         return "main";
     }
+
+    @GetMapping("/lost-pw")
+    public String lostPw() {
+        return "lostPw";
+    }
+
+    @PostMapping("/mail-send")
+    public String sendMail(MailDto mailDto, Model model) {
+        if(mailService.mailSend(mailDto)){
+            return "mailDone"; // 아이디가 존재할 경우
+        }else {
+            model.addAttribute("error","존재하지 않는 아이디입니다.");
+            return "lostPw";   // 아이디가 존재하지 않을 경우
+        }
+    }
+
 }
