80cebe69def84a8d5dd6138d7169caf406b1bcc1
kim <ksh251024@naver.com>
2024-09-30 14:55:05 +0900

    feat : 필드 초기화 방식 개선, 필드명 변경

--- src/main/java/com/copypay/dto/MailDto.java
+++ src/main/java/com/copypay/dto/MailDto.java
@@ -6,6 +6,14 @@ import lombok.Data;
 @Data
 public class MailDto {
     private String id;
-    private String title = "Copy Pay 임시 비밀번호입니다";
-    private String content  = RandomPassword.getRandomPassword(13);
+    private String title;
+    private String content;
+
+    private static final String DEFAULT_TITLE = "Copy Pay 임시 비밀번호입니다";
+    private static final int PASSWORD_LENGTH = 13;
+
+    public MailDto() {
+        this.title = DEFAULT_TITLE;
+        this.content = RandomPassword.getRandomPassword(PASSWORD_LENGTH);
+    }
 }
--- src/main/java/com/copypay/util/RandomPassword.java
+++ src/main/java/com/copypay/util/RandomPassword.java
@@ -3,7 +3,7 @@ package com.copypay.util;
 import java.security.SecureRandom;
 
 public class RandomPassword {
-    private static final char[] rndAllCharacters = new char[]{
+    private static final char[] ALLOWED_PASSWORD_CHARACTERS = new char[]{
             '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
             'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
@@ -15,9 +15,9 @@ public class RandomPassword {
     public static String getRandomPassword(int length) {
         SecureRandom random = new SecureRandom();
         StringBuilder stringBuilder = new StringBuilder();
-        int rndAllCharactersLength = rndAllCharacters.length;
+        int allowedPasswordCharactersLength = ALLOWED_PASSWORD_CHARACTERS.length;
         for (int i = 0; i < length; i++) {
-            stringBuilder.append(rndAllCharacters[random.nextInt(rndAllCharactersLength)]);
+            stringBuilder.append(ALLOWED_PASSWORD_CHARACTERS[random.nextInt(allowedPasswordCharactersLength)]);
         }
         return stringBuilder.toString();
     }
