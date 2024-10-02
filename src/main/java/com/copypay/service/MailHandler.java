c6e98e60951d79549f7a48d6eb444a0ff53c9054
kim <ksh251024@naver.com>
2024-10-02 14:36:48 +0900

    feat : @Slf4j 어노테이션 사용

--- src/main/java/com/copypay/service/MailHandler.java
+++ src/main/java/com/copypay/service/MailHandler.java
@@ -2,14 +2,13 @@ package com.copypay.service;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.MailSendException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 
+@Slf4j
 public class MailHandler {
-    private static final Logger logger = LoggerFactory.getLogger(MailHandler.class);
     private final JavaMailSender sender;
     private final MimeMessage message;
     private final MimeMessageHelper messageHelper;
@@ -37,10 +36,10 @@ public class MailHandler {
         try {
             sender.send(message);
         } catch (MailSendException e) { // 메일 전송 중 발생한 MailSendException 처리
-            logger.error("메일 전송 실패: {}", e.getMessage());
+            log.error("메일 전송 실패: {}", e.getMessage());
             throw e;  // 컨트롤러로 예외 던짐
         } catch (Exception e) {         // 그 외 예외 처리
-            logger.error("메일 전송 중 예외 발생: {}", e.getMessage());
+            log.error("메일 전송 중 예외 발생: {}", e.getMessage());
             throw new MailSendException("메일 전송 중 예외 발생", e);  // 예외를 MailSendException으로 변환해서 던져줌
         }
     }
