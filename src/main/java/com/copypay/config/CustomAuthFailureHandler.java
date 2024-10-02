f2141a347bc25de7ba8f40b0e64849dcfa8ff702
kim <ksh251024@naver.com>
2024-10-02 13:19:43 +0900

    comment : 주석 추가

--- src/main/java/com/copypay/config/CustomAuthFailureHandler.java
+++ src/main/java/com/copypay/config/CustomAuthFailureHandler.java
@@ -6,9 +6,10 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
-
 import java.io.IOException;
-
+/*
+    로그인 실패 시 이동할 url 지정을 위한 클래스
+*/
 @Component
 public class CustomAuthFailureHandler extends SimpleUrlAuthenticationFailureHandler {
     @Override
