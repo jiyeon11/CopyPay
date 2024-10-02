547f21603ecfea9f61c2d56394c9ee232c4e2ec0
kim <ksh251024@naver.com>
2024-10-02 13:50:00 +0900

    feat : @Slf4j 어노테이션 사용

--- src/main/java/com/copypay/config/CustomAuthSuccessListener.java
+++ src/main/java/com/copypay/config/CustomAuthSuccessListener.java
@@ -1,20 +1,18 @@
 package com.copypay.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.stereotype.Component;
 /*
     로그인 성공 시 username 출력을 위한 클래스
 */
+@Slf4j
 @Component
 public class CustomAuthSuccessListener implements ApplicationListener<AuthenticationSuccessEvent> {
-    private static final Logger logger = LoggerFactory.getLogger(CustomAuthSuccessListener.class);
-
     @Override
     public void onApplicationEvent(AuthenticationSuccessEvent event) {
         String username = event.getAuthentication().getName();
-        logger.info("Success authentication attempt for user: {}", username);
+        log.info("Success authentication attempt for user: {}", username);
     }
 }
--- src/main/java/com/copypay/controller/LoginController.java
+++ src/main/java/com/copypay/controller/LoginController.java
@@ -3,8 +3,7 @@ package com.copypay.controller;
 import com.copypay.dto.MailDto;
 import com.copypay.service.MailService;
 import lombok.AllArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.MailSendException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,10 +11,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
+@Slf4j
 @AllArgsConstructor
+@Controller
 public class LoginController {
-    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
     private final MailService mailService;
 
     @GetMapping("/")
@@ -46,7 +45,7 @@ public class LoginController {
             model.addAttribute("error", "메일 전송 중 오류가 발생했습니다.");
             return "lostPw";
         } catch (Exception e) { // 그 외 예외 처리
-            logger.error("예외 발생: {}", e.getMessage());
+            log.error("예외 발생: {}", e.getMessage());
             model.addAttribute("error", "메일 전송 중 오류가 발생했습니다.");
             return "lostPw";
         }
