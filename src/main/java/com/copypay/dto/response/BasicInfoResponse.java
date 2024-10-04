7efea9f178718912eda3378a285e1d63dcc06d74
jiyeon11 <leejiyeon415@gmail.com>
2024-10-04 13:53:27 +0900

    feat : 사업자번호로 조회하여 정산정보 입력 필드 업데이트

--- src/main/java/com/copypay/dto/response/BasicInfoResponse.java
+++ src/main/java/com/copypay/dto/response/BasicInfoResponse.java
@@ -7,6 +7,7 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class BasicInfoResponse {
+    /* 계약 */
     private String businessRegNumber;  //사업자번호
     private String mid;  //MID
     private String businessName;  //상호
@@ -30,4 +31,14 @@ public class BasicInfoResponse {
     private String applicationPath;  //접수경로
     private String contractManager;  //계약담당자
     private String salesManager;  //영업담당자
+    /* 정산정보 */
+    private String paymentMethod;  //지급방법
+    private String feePaymentMethod;  //수수료 지급방법
+    private String feeSettlementDate;  //수수료 정산일자
+    private String depositAccountFormat;  //정산 입금 통장 인자 표기 방법
+    private String accountParameterName;  //통장 인자명
+    private String taxInvoiceCriteria;  //세금계산서 발행 기준
+    private String taxInvoiceVat;  //세금계산서 부가세 계산 방식
+    private String taxInvoiceEmail;  //세금계산서 이메일
+    private String partialCancellationAllowed;  //부분취소 가능 여부
 }
