0c02677d67fdf64ca6e1c98428ce77cdad9e7f8b
kim <ksh251024@naver.com>
2024-10-17 14:41:10 +0900

    feat : ContractProgressController 계약 신규등록 엔드포인트 추가

--- src/main/java/com/copypay/controller/ContractProgressController.java
+++ src/main/java/com/copypay/controller/ContractProgressController.java
@@ -1,14 +1,13 @@
 package com.copypay.controller;
 
+import com.copypay.dto.request.ContractRegisterRequest;
 import com.copypay.dto.response.ContractProgressListResponse;
 import com.copypay.service.ContractProgressService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,13 +21,17 @@ public class ContractProgressController {
     public String contract_progress(){
         return "contract-progress";
     }
+
     @GetMapping(value = {"/api/contract-progress/list/{checkedDate}/{startDate}/{endDate}", "/api/contract-progress/list"})
     @ResponseBody
     public ResponseEntity<List<ContractProgressListResponse>> getContractProgressList(@PathVariable(required = false) String checkedDate, @PathVariable(required = false) String startDate, @PathVariable(required = false) String endDate){
-        System.out.println("시작날짜 :  "+startDate);
-        System.out.println("마지막날짜 :  "+endDate);
-        System.out.println("chk :  "+checkedDate);
         return ResponseEntity.ok(contractProgressService.getContractProgressList(checkedDate, startDate, endDate));
+    }
+
+    @PostMapping("/api/contract-progress/register")
+    public String contract_progress_register(ContractRegisterRequest contractRegisterRequest){
+        contractProgressService.registerContract(contractRegisterRequest);
+        return "main";
     }
 
 }
