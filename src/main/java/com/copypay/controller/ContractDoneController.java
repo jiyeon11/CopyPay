8006ff725f62d5784a45bd4d6d8699bff17879b9
kim <ksh251024@naver.com>
2024-10-18 10:53:31 +0900

    feat : ContractDoneController 계약 완료현황 엔드포인트 추가

---
+++ src/main/java/com/copypay/controller/ContractDoneController.java
@@ -0,0 +1,29 @@
+package com.copypay.controller;
+
+import com.copypay.dto.response.ContractDoneListResponse;
+import com.copypay.service.SalesManagementService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class ContractDoneController {
+    private final SalesManagementService salesManagementService;
+
+    @GetMapping("/contract-done")
+    public String contract_done() {
+        return "contract-done";
+    }
+
+    @GetMapping(value = {"/api/contract-done/list/{searchOption}/{searchValue}", "/api/contract-done/list"})
+    @ResponseBody
+    public ResponseEntity<List<ContractDoneListResponse>> getContractDoneList(@PathVariable(required = false) String searchOption, @PathVariable(required = false) String searchValue){
+        return ResponseEntity.ok(salesManagementService.getContractDoneList(searchOption, searchValue));
+    }
+}
