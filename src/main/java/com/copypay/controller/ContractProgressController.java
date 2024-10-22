48064ab269c73b5d850c46c8b7789b5aa23a0607
kim <ksh251024@naver.com>
2024-10-22 09:42:35 +0900

    feat : 계약 신규등록 시 @Valid로 사용자 입력 값 검증

--- src/main/java/com/copypay/controller/ContractProgressController.java
+++ src/main/java/com/copypay/controller/ContractProgressController.java
@@ -2,13 +2,13 @@ package com.copypay.controller;
 
 import com.copypay.dto.request.ContractRegisterRequest;
 import com.copypay.dto.response.ContractProgressListResponse;
-import com.copypay.service.ContractProgressService;
+import com.copypay.service.SalesManagementService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @Controller
@@ -18,20 +18,20 @@ public class ContractProgressController {
     private final SalesManagementService salesManagementService;
 
     @GetMapping("/contract-progress")
-    public String contract_progress(){
+    public String contract_progress() {
         return "contract-progress";
     }
 
     @GetMapping(value = {"/api/contract-progress/list/{checkedDate}/{startDate}/{endDate}", "/api/contract-progress/list"})
     @ResponseBody
-    public ResponseEntity<List<ContractProgressListResponse>> getContractProgressList(@PathVariable(required = false) String checkedDate, @PathVariable(required = false) String startDate, @PathVariable(required = false) String endDate){
+    public ResponseEntity<List<ContractProgressListResponse>> getContractProgressList(@PathVariable(required = false) String checkedDate, @PathVariable(required = false) String startDate, @PathVariable(required = false) String endDate) {
         return ResponseEntity.ok(salesManagementService.getContractProgressList(checkedDate, startDate, endDate));
     }
 
     @PostMapping("/api/contract-progress/register")
-    public String contract_progress_register(ContractRegisterRequest contractRegisterRequest){
-        contractProgressService.registerContract(contractRegisterRequest);
-        return "main";
+    @ResponseBody
+    public ResponseEntity<?> contract_progress_register(@Valid @RequestBody ContractRegisterRequest contractRegisterRequest) {
+        salesManagementService.registerContract(contractRegisterRequest);
+        return ResponseEntity.ok().build();
     }
-
 }
--- src/main/java/com/copypay/dto/request/ContractRegisterRequest.java
+++ src/main/java/com/copypay/dto/request/ContractRegisterRequest.java
@@ -1,28 +1,45 @@
 package com.copypay.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class ContractRegisterRequest {
     private String applicationPath; // 접수경로
+    @NotBlank(message = "상호를 입력해주세요.")
     private String businessName; // 상호
     private String businessType; // 사업체 구분
+    @NotBlank(message = "사업자 번호를 입력해주세요.")
     private String businessRegNumber; // 사업자 번호
+    @NotBlank(message = "대표자명을 입력해주세요.")
     private String ceoName; //
+    @NotBlank(message = "대표자 생년월일을 입력해주세요.")
     private String ceoBirth; // 대표자 생년월일
+    @NotBlank(message = "대표자 HP를 입력해주세요.")
     private String ceoHp; // 대표자 HP
+    @NotBlank(message = "대표자 TEL을 입력해주세요.")
     private String ceoTel; // 대표자 Tel
+    @NotBlank(message = "대표자 E-Mail을 입력해주세요.")
     private String ceoEmail; // 대표자 Email
     private String ceoFax; // 대표자 FAX
+    @NotBlank(message = "사업장 소재지를 입력해주세요.")
     private String businessPostCode; // 사업장 소재지 우편번호
+    @NotBlank(message = "사업장 소재지를 입력해주세요.")
     private String businessAddressLine1; // 사업장 소재지 주소1
+    @NotBlank(message = "사업장 소재지를 입력해주세요.")
     private String businessAddressLine2; // 사업장 소재지 주소2
+    @NotBlank(message = "가맹점 계약담당자명을 입력해주세요.")
     private String franchiseContractManagerName; // 가맹점 계약담당자 이름
+    @NotBlank(message = "가맹점 계약담당자 Tel을 입력해주세요.")
     private String franchiseContractManagerTel; // 가맹점 계약담당자 Tel
+    @NotBlank(message = "가맹점 계약담당자 HP를 입력해주세요.")
     private String franchiseContractManagerHp; // 가맹점 계약담당자 Hp
+    @NotBlank(message = "가맹점 계약담당자 E-Mail을 입력해주세요.")
     private String franchiseContractManagerEmail; // 가맹점 계약담당자 Email
+    @NotBlank(message = "세금계산서 E-Mail을 입력해주세요.")
     private String taxInvoiceEmail; // 세금계산서 E-Mail
     private String contractManager; // 계약담당자
     private String salesManager; // 영업담당자
+    @NotBlank(message = "계약일자를 입력해주세요.")
     private String contractDate; // 계약일자
 }
