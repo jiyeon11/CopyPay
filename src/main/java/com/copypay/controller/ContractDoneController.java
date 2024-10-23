69f1639d548ffb9291d026aa7d63ce08da16a648
kim <ksh251024@naver.com>
2024-10-23 09:12:07 +0900

    fix : @PathVariable에서 @RequestParam으로 변경

--- src/main/java/com/copypay/controller/ContractDoneController.java
+++ src/main/java/com/copypay/controller/ContractDoneController.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -21,9 +21,9 @@ public class ContractDoneController {
         return "contract-done";
     }
 
-    @GetMapping(value = {"/api/contract-done/list/{searchOption}/{searchValue}", "/api/contract-done/list"})
+    @GetMapping("/api/contract-done/list")
     @ResponseBody
-    public ResponseEntity<List<ContractDoneListResponse>> getContractDoneList(@PathVariable(required = false) String searchOption, @PathVariable(required = false) String searchValue){
+    public ResponseEntity<List<ContractDoneListResponse>> getContractDoneList(@RequestParam(required = false) String searchOption, @RequestParam(required = false) String searchValue){
         return ResponseEntity.ok(salesManagementService.getContractDoneList(searchOption, searchValue));
     }
 }
