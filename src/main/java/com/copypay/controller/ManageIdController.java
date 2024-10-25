14a0ef9a32758ce758e03263ff533311b20698e8
kim <ksh251024@naver.com>
2024-10-25 11:54:36 +0900

    feat : 가맹점 ID 관리 조회 엔드포인트 추가

--- src/main/java/com/copypay/controller/ManageIdController.java
+++ src/main/java/com/copypay/controller/ManageIdController.java
@@ -1,17 +1,31 @@
 package com.copypay.controller;
 
+import com.copypay.dto.response.ManageIdListResponse;
+import com.copypay.service.SalesManagementService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class ManageIdController {
+    private final SalesManagementService salesManagementService;
 
     @GetMapping("/manage-id")
     public String manageId() {
         return "manage-id";
     }
 
+    @GetMapping("/api/manage-id/list")
+    @ResponseBody
+    public ResponseEntity<List<ManageIdListResponse>> getManageIdList(@RequestParam(required = false) String searchOption, @RequestParam(required = false) String searchValue){
+        return ResponseEntity.ok(salesManagementService.getManageIdList(searchOption, searchValue));
+    }
 
 }
