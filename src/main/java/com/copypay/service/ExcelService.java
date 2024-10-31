406b02ba15c378c206ffd84d0e8d881eb06edf13
kim <ksh251024@naver.com>
2024-10-31 10:35:24 +0900

    design : 셀 스타일 지정

--- src/main/java/com/copypay/service/ExcelService.java
+++ src/main/java/com/copypay/service/ExcelService.java
@@ -22,21 +22,34 @@ public class ExcelService {
             // 엑셀 파일 헤더 생성
             String[] headers = {"계약일자", "사업자번호", "상호", "계약담당자", "영업담당자"};
             for (int i = 0; i < headers.length; i++) {
-                sheet.setColumnWidth(i,5000); // 셀 가로폭 지정
+                CellStyle cellStyle = createHeaderStyle(workbook);
+                sheet.setColumnWidth(i, 6000); // 셀 가로폭 지정
                 Cell cell = headerRow.createCell(i);
                 cell.setCellValue(headers[i]);
+                cell.setCellStyle(cellStyle);
             }
 
             // 데이터가 있을 때만 추가
             if (!contractProgressList.isEmpty()) {
+                CellStyle cellStyle = createStyle(workbook);
                 int rowIndex = 1;
                 for (ContractProgressListResponse contract : contractProgressList) {
                     Row row = sheet.createRow(rowIndex++);
-                    row.createCell(0).setCellValue(contract.getContractDate());
-                    row.createCell(1).setCellValue(contract.getBusinessRegNumber());
-                    row.createCell(2).setCellValue(contract.getBusinessName());
-                    row.createCell(3).setCellValue(contract.getContractManager());
-                    row.createCell(4).setCellValue(contract.getSalesManager());
+                    Cell dateCell = row.createCell(0);
+                    dateCell.setCellValue(contract.getContractDate());
+                    dateCell.setCellStyle(cellStyle);
+                    Cell businessRegNumber = row.createCell(1);
+                    businessRegNumber.setCellValue(contract.getBusinessRegNumber());
+                    businessRegNumber.setCellStyle(cellStyle);
+                    Cell businessName = row.createCell(2);
+                    businessName.setCellValue(contract.getBusinessName());
+                    businessName.setCellStyle(cellStyle);
+                    Cell contractManager = row.createCell(3);
+                    contractManager.setCellValue(contract.getContractManager());
+                    contractManager.setCellStyle(cellStyle);
+                    Cell salesManager = row.createCell(4);
+                    salesManager.setCellValue(contract.getSalesManager());
+                    salesManager.setCellStyle(cellStyle);
                 }
             }
 
@@ -49,5 +62,19 @@ public class ExcelService {
         }
     }
 
+    // 헤더 스타일 지정
+    private CellStyle createHeaderStyle(Workbook workbook) {
+        CellStyle cellStyle = workbook.createCellStyle();
+        cellStyle.setFillForegroundColor(IndexedColors.YELLOW.getIndex());
+        cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        cellStyle.setAlignment(HorizontalAlignment.CENTER);
+        return cellStyle;
+    }
+    // 데이터 목록 스타일 지정
+    private CellStyle createStyle(Workbook workbook) {
+        CellStyle cellStyle = workbook.createCellStyle();
+        cellStyle.setAlignment(HorizontalAlignment.CENTER);
+        return cellStyle;
+    }
 
 }
