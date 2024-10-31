830ff3ebb0c533da21546efc44c42471c01066a2
kim <ksh251024@naver.com>
2024-10-31 13:46:16 +0900

    move : 파일 위치를 listener 패키지로 변경

--- src/main/java/com/copypay/listener/CustomAuthFailureListener.java
+++ src/main/java/com/copypay/listener/CustomAuthFailureListener.java
@@ -1,4 +1,4 @@
-package com.copypay.config;
+package com.copypay.listener;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
--- src/main/java/com/copypay/listener/CustomAuthSuccessListener.java
+++ src/main/java/com/copypay/listener/CustomAuthSuccessListener.java
@@ -1,4 +1,4 @@
-package com.copypay.config;
+package com.copypay.listener;
 
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
